26c6220d18cedf8b262f20f9a2234829f0c50d52
Daniyal Aamir <daamir89@gmail.com>
2024-06-25 11:56:38 -0400

    implement character count

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,16 +1,23 @@
+import { useState } from "react"
+import { MAX_CHARACTERS } from "../lib/constants"
+
 export default function FeedbackForm() {
+  const [text, setText] = useState("")
+  const charCount = MAX_CHARACTERS - text.length
   return (
     <form className="form">
       <textarea 
         id="feedback-textarea"
         placeholder=""
         spellCheck={false}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
       />
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to #hashtag the company
       </label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
